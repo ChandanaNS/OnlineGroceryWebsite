f7f8b01652daecdd46013f5b4db45709866bcfd8
Chandana Aradhya <60341885+ChandanaNS@users.noreply.github.com>
2020-07-13 16:41:20 +0100

    script clean

--- DBResources/GroceryGo.sql
+++ DBResources/GroceryGo.sql
@@ -28,31 +28,10 @@ CREATE TABLE IF NOT EXISTS products (
     Discount Int(3)
 )  ENGINE=INNODB;
 
--- orders table
--- CREATE TABLE IF NOT EXISTS orders (
---     OrderID INT(11) AUTO_INCREMENT PRIMARY KEY,
---     UserID INT(11) ,
---     FOREIGN KEY (UserID) REFERENCES users(UserID)
--- )  ENGINE=INNODB;
-
--- CREATE TABLE IF NOT EXISTS orderItemMap (
---     OrderItemID INT(11) AUTO_INCREMENT PRIMARY KEY,
---     UserID INT(11) ,
---     ProductID INT(11) ,
---     FOREIGN KEY (UserID) REFERENCES users(UserID),
---     FOREIGN KEY (ProductID) REFERENCES products(ProductID)
--- )  ENGINE=INNODB;
 
 -- DELETE script test
 DELETE FROM products WHERE ProductID=1;
--- delete from users;
--- select * from users;
 
--- set foreign_key_checks=0;
--- truncate table users;
--- set foreign_key_checks=1;
-
-SELECT * FROM products;
 INSERT INTO products VALUES (1,'Nestle','Dairy','Milk','500Ml','',1.5,0);
 INSERT INTO products VALUES (2,'Lidl','Dairy','Milk','500Ml','',1.5,0);
 INSERT INTO products VALUES (3,'Banana','Fresh','Fruits','5 Pieces','https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80',1.29,0);
@@ -64,12 +43,6 @@ INSERT INTO products VALUES (8,'Banana','Fresh','Fruits','5 Pieces','https://ima
 INSERT INTO products VALUES (9,'Banana','Fresh','Fruits','10 Pieces','https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80',1.29,0);
 INSERT INTO products VALUES (10,'Aldi','Dairy','Milk','500Ml','https://images.unsplash.com/photo-1550583724-b2692b85b150?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',2,0);
 
--- update test
-UPDATE products
-SET
-    Image = 'https://images.unsplash.com/photo-1557800636-894a64c1696f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2801&q=80'
-WHERE
-    ProductID = 11;
 
  -- added wallet column
 ALTER TABLE users
@@ -79,13 +52,3 @@ ADD COLUMN Wallet VARCHAR(20) ;
 ALTER TABLE users
 ADD COLUMN OrderedProducts longtext;
 
--- ALTER TABLE users CHANGE orderedProducts OrderedProducts longtext;
-
---  -- added order details column
--- ALTER TABLE orders
--- ADD COLUMN ProductId Int(11),ADD COLUMN NumberOfItems Int(11) ;
-
--- Select * from users;
-
--- SELECT * FROM products where ProductID=1
--- UPDATE users SET Wallet = '50' ;
