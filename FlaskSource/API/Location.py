89ce7ff3f01f583cb9ed917bd330a5c259694da5
ChandanaNS <10539269@mydbs.ie>
2020-07-13 05:17:21 +0100

    Location API update

--- FlaskSource/API/Location.py
+++ FlaskSource/API/Location.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-
 from flask import render_template, session, redirect, Blueprint, flash, jsonify, logging, request
-from flask_simple_geoip import SimpleGeoIP
 import requests
 from Database import DBQuery
 import json
@@ -18,11 +16,11 @@ def getLocation():
         location_data = {}
         if request.method == "POST":
             userOrderList = []
-
             if request.get_json():
                 userOrderHistory = DBQuery.getUserByUserName(session.get("userName"))
                 ajaxData = json.loads(request.get_json()['data'])
                 session['walletBalance'] = request.get_json()['wallet']
+                # post  location
                 if userOrderHistory[9] is None:
                     userOrderList = []
                 else:
@@ -36,7 +34,6 @@ def getLocation():
                                                 order['count'],
                                                 session.get("userName"),
                                                 datetime.now().strftime('%Y-%m-%dT%H:%M:%S'))
-
                     userOrderList.append(orderDetails.__dict__)
                 session['userOderList'] = userOrderList
                 # Sending back AJAX Response Call
@@ -56,29 +53,26 @@ def getLocation():
                     # sending get request and saving the response as response object
                     r = requests.get(url=URL, params=PARAMS)
                     data = r.json()
-                    # print(data,"latlang")
 
                     # Acquiring the latitude and longitude from JSON
                     latitude = data['items'][0]['position']['lat']
-                    # print(latitude)
                     longitude = data['items'][0]['position']['lng']
-                    # print(longitude)
                     location_data['data'] = data
                     location_data['api_key'] = api_key
                     location_data['latitude'] = latitude
                     location_data['longitude'] = longitude
                     location_data['postAction'] = postAction
                     location_data['address'] = address
-                    # print("location ", location_data)
                     return render_template('location.html', data=location_data)
                 elif postAction == 'confirm':
-                    userOrderList = session.get('userOderList');
+                    userOrderList = session.get('userOderList')
                     DBQuery.updateOrders(json.dumps(userOrderList), session.get("userName"))
                     DBQuery.walletBalance(session.get('walletBalance'), session.get('userName'))
                     # userDetailsFromDB = DBQuery.getUserByUserName(session.get('userName'))
                     session['user'][6] = session.get('walletBalance')
                     flash('Order has been placed successfully! Continue shopping')
                     return redirect('/shop')
+        # Default ballsbridge dublin location data
         location_data = {'data': {'items': [{'title': 'Ballsbridge, Dublin, Ireland', 'id': 'here:cm:namedplace:23217387', 'resultType': 'locality', 'localityType': 'district', 'address': {'label': 'Ballsbridge, Dublin, Ireland', 'countryCode': 'IRL', 'countryName': 'Ireland', 'county': 'County Dublin', 'city': 'Dublin', 'district': 'Ballsbridge', 'postalCode': '4'}, 'position': {'lat': 53.33061, 'lng': -6.23343}, 'mapView': {'west': -6.25266, 'south': 53.31556, 'east': -6.20429, 'north': 53.3392}, 'scoring': {'queryScore': 1.0, 'fieldScore': {'district': 1.0}}}]}, 'api_key': 'oILvnb3yIsakFmQI-UJafpCXmRvN4INE2EZSwgt0R8s', 'latitude': 53.33061, 'longitude': -6.23343}
         return render_template('location.html', data=location_data)
     except Exception as e:
